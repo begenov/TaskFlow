e53ade9036c2a7cef3ca8726c79d9e612b68c61e
Your Name <you@example.com>
2023-04-16 17:17:36 +0600

    gin learn gramework go

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,13 +1,9 @@
 package app
 
-import (
-	"github.com/begenov/TaskFlow/internal/pkg/lib/e"
-	"github.com/begenov/TaskFlow/internal/storage"
-)
-
 func Run() error {
-	storage, err := storage.NewStorage()
-	if err != nil {
-		return e.Wrap("", err)
-	}
+	// storage, err := storage.NewStorage()
+	// if err != nil {
+	// 	return e.Wrap("", err)
+	// }
+	return nil
 }
