c7df6d38e69463d56403a2bae4c27212e327e7b4
Your Name <you@example.com>
2023-04-20 11:19:46 +0600

    remove

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,4 @@
 package main
 
-import (
-	"github.com/begenov/TaskFlow/cmd/controller"
-	"github.com/begenov/TaskFlow/cmd/service"
-	"github.com/gin-gonic/gin"
-)
-
-var (
-	videoService    service.VedioService       = service.New()
-	VideoController controller.VideoController = controller.New(videoService)
-)
-
 func main() {
-	mux := gin.Default()
-	mux.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, VideoController.FindAll())
-
-	})
-
-	mux.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, VideoController.Save(ctx))
-
-	})
-
-	mux.Run(":8080")
 }
