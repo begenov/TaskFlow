036d24918758e27d1061f69c0f98a45f309e0b31
begenov <begenovorazali@icloud.com>
2023-05-08 09:45:52 +0600

    config file

--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
--- user-app/internal/config/config.go
+++ user-app/internal/config/config.go
@@ -1,18 +1,47 @@
 package config
 
-import "time"
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/subosito/gotenv"
+)
 
 type (
 	Config struct {
-		JWT JWTConfig
+		JWT jwt_config
 	}
 
-	JWTConfig struct {
+	jwt_config struct {
 		AccessTokenTTL  time.Duration
 		RefreshTokenTTL time.Duration
 		SigninKey       string
 	}
 )
 
-func NewConfig() {
+func NewConfig() (*Config, error) {
+	err := gotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	accessTokenTTL, err := strconv.ParseInt(os.Getenv("ACCESS_TOKEN_TTL"), 10, 64)
+	if err != nil {
+		log.Fatalf("Error parsing ACCESS_TOKEN_TTL: %v", err)
+	}
+
+	refreshTokenTTL, err := strconv.ParseInt(os.Getenv("REFRESH_TOKEN_TTL"), 10, 64)
+	if err != nil {
+		log.Fatalf("Error parsing REFRESH_TOKEN_TTL: %v", err)
+	}
+
+	return &Config{
+		JWT: jwt_config{
+			AccessTokenTTL:  time.Duration(accessTokenTTL) * time.Minute,
+			RefreshTokenTTL: time.Duration(refreshTokenTTL) * time.Hour,
+			SigninKey:       os.Getenv("SIGNIN_KEY"),
+		},
+	}, nil
 }
