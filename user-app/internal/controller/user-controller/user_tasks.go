413e5cb074aabb1c9c8d510de7942794e77ab54c
begenov <begenovorazali@icloud.com>
2023-05-14 09:26:16 +0600

    pix

--- user-app/internal/controller/user-controller/user_tasks.go
+++ user-app/internal/controller/user-controller/user_tasks.go
@@ -1,21 +1,39 @@
 package usercontroller
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
+	"github.com/begenov/TaskFlow/pkg/models"
 	"github.com/gin-gonic/gin"
 )
+
+type res struct {
+	Task []models.Todo `json:"successful"`
+}
+
+type aa struct {
+	Res res `json:"successful"`
+}
 
 func (u *UserController) UserAllTasks(ctx *gin.Context) {
 	resp, err := http.Get("http://localhost:8000/tasks")
 	if err != nil {
 		return
 	}
-	body, err := io.ReadAll(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
 	}
-	fmt.Println(string(body))
+	var task []models.Todo
+
+	if err := json.NewDecoder(resp.Body).Decode(&task); err != nil {
+		log.Fatal(string(body), err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, task)
 }
